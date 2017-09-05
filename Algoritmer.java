507a1680d0b447d96c96aa95d2699e85b321d9d8
bjornkpu <bjornkpu@stud.ntnu.no>
2017-09-05 13:24:19 +0200

    second commit

---
+++ Algoritmer.java
@@ -0,0 +1,11 @@
+package Øvinger.Øving3_Sortering;
+
+/**
+ * @author Bjørn Kristian Punsvik
+ */
+public class Algoritmer {
+	public static void shellsort(int[]t){
+		int s = t.length/2;
+		while(s > 0)
+	}
+}
---
+++ Main.java
@@ -0,0 +1,7 @@
+package Øvinger.Øving3_Sortering;
+
+/**
+ * @author Bjørn Kristian Punsvik
+ */
+public class Main {
+}
