4c9782b2eaf0efffab51228787098e5150e40e6b
Johan Martin Skavhaug <johanmsk@stud.ntnu.no>
2017-09-05 14:46:24 +0200

    added comments to quicksort

--- Algoritme.java
+++ Algoritme.java
@@ -4,7 +4,7 @@ public class Algoritme {
     static int[] numbers;
     static int number;
 
-    public static int[] sort(int[] values){
+    public static int[] sort(int[] values, int deleverdi){
         if(values.length < 1 || values == null) return null;
 
         numbers = values;
@@ -14,22 +14,36 @@ public class Algoritme {
         return numbers;
     }
 
-    private static void quicksort(int low, int high){
+    private static void quicksort(int low, int high) {
         int i = low, j = high;
+        // Get the pivot element from the middle of the list
         int pivot = numbers[low + (high-low)/2];
+
+        // Divide into two lists
         while (i <= j) {
+            // If the current value from the left list is smaller than the pivot
+            // element then get the next element from the left list
             while (numbers[i] < pivot) {
                 i++;
             }
+            // If the current value from the right list is larger than the pivot
+            // element then get the next element from the right list
             while (numbers[j] > pivot) {
                 j--;
             }
+
+            // If we have found a value in the left list which is larger than
+            // the pivot element and if we have found a value in the right list
+            // which is smaller than the pivot element then we exchange the
+            // values.
+            // As we are done we can increase i and j
             if (i <= j) {
                 exchange(i, j);
                 i++;
                 j--;
             }
         }
+        // Recursion
         if (low < j)
             quicksort(low, j);
         if (i < high)
